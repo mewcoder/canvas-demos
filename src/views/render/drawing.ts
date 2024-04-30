4542fb8daf3551a2e7608cb30df1305f3c4a2dd0
mewcoder <1010953107.com>
2024-04-30 18:42:33 +0800

    feat: add test

--- src/views/render/drawing.ts
+++ src/views/render/drawing.ts
@@ -5,10 +5,12 @@ export class Drawing {
   private ctx: CanvasRenderingContext2D;
   private startPoint: Point = { x: 0, y: 0 };
   private drawingRect: any = null;
+  private rects: Rectangle[] = [];
 
   constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.ctx = ctx;
+    this.rects = [];
     this.addEvents();
   }
 
@@ -36,7 +38,7 @@ export class Drawing {
     this.drawingRect = null;
     const width = Math.abs(e.offsetX - this.startPoint.x);
     const height = Math.abs(e.offsetY - this.startPoint.y);
-    if (width < 1 && height < 1) return;
+    if (width < 1 || height < 1) return;
     const x = Math.min(this.startPoint.x, e.offsetX);
     const y = Math.min(this.startPoint.y, e.offsetY);
 
@@ -47,11 +49,17 @@ export class Drawing {
       height,
     };
 
-    //   rects.push(newRect);
+    this.rects.push(newRect);
   };
 
   draw() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.rects.forEach((rect) => {
+      this.ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    });
+
     if (!this.drawingRect) return;
+
     const { x, y, width, height } = this.drawingRect;
     this.ctx.strokeRect(x, y, width, height);
   }
